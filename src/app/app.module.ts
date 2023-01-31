154b1c0fbd54b03fcf5265db5a606ad29af932cf
vhlahti <henri.veholahti@gmail.com>
2023-01-31 11:07:47 +0200

    add component display-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayDetailsComponent } from './display-details/display-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/display-details/display-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-details',
+  templateUrl: './display-details.component.html',
+  styleUrls: ['./display-details.component.css']
+})
+export class DisplayDetailsComponent {
+
+}
